f50ce17589f48540c8bd09dc9be5ee9215409418
Jurandir76 <jurandy002junior@gmail.com>
2024-12-18 16:49:02 -0300

    exercicio atualizados

--- exercicio 06.py
+++ exercicio 06.py
@@ -2,4 +2,8 @@ n = int(input('Digite um numero! '))
 d = n *2
 t = n *3
 r = n**(1/2)
-print('O duplo de {} é {} o triplo é {} e sua raiz quadrada é correspondente a {} '.format(n, d, t, r) )
+print('O duplo de {} é {} o triplo é {} e sua raiz quadrada é correspondente a {:.2f} '.format(n, d, t, r) )
+
+#
+nu = int(input('Digite um numero! '))
+print('O dobro de {} é {}! \nO triplo é {}!\nA raiz quadrada é {}!' .format( nu, (nu*2), (nu*3), (nu **(1/2)) ))
