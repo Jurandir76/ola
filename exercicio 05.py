c6ea3f6b0a89d5720a4dce2aa4482c7825834729
Jurandir76 <jurandy002junior@gmail.com>
2024-12-18 16:05:38 -0300

    exercicio atualizados

--- exercicio 05.py
+++ exercicio 05.py
@@ -4,4 +4,4 @@ s = n+1
 print('Qual é o antecessor de {} e o sucessor de {} '.format( a, s, n))
 
 numero = int(input('Digite um numero! '))
-print('O numero que antecede {}, é {} e o seu sucessor é {} !' .format(numero, (numero-1), (numero+1)))
+print('O numero que antecede {}, é {} e o seu sucessor é {} !'  .format(numero, (numero-1), (numero+1)))
